72eeabd7c6c489ed646b95ceb4aed522ef7671b3
juSt-jeLLy <yagneshpatel931@gmail.com>
2026-03-19 14:48:55 +0530

    Initialize FCL config before render

--- frontend/src/components/flow-provider-wrapper.tsx
+++ frontend/src/components/flow-provider-wrapper.tsx
@@ -2,7 +2,7 @@
 
 import { FlowProvider } from "@onflow/react-sdk";
 import * as fcl from "@onflow/fcl";
-import { ReactNode, useEffect } from "react";
+import { ReactNode } from "react";
 import flowJSON from "../../flow.json";
 import { flowConfig } from "@/flow/config";
 
@@ -11,14 +11,14 @@ interface FlowProviderWrapperProps {
 }
 
 export function FlowProviderWrapper({ children }: FlowProviderWrapperProps) {
-  useEffect(() => {
+  if (typeof window !== "undefined") {
     fcl
       .config()
       .put("accessNode.api", flowConfig.accessNodeUrl)
       .put("discovery.wallet", flowConfig.discoveryWallet)
       .put("discovery.authn.endpoint", flowConfig.discoveryAuthnEndpoint)
       .put("flow.network", flowConfig.flowNetwork);
-  }, []);
+  }
 
   return (
     <FlowProvider
